7cdc492a9ac46792536079623d0ecc6ff334f8bb
sebastian <sebmuell@phys.ethz.ch>
2016-07-28 12:38:48 +0200

    throw meaningfull exception when CORSIKA input card can not be parsed, use os.path.join over join

--- corsika_iact_caller/__init__.py
+++ corsika_iact_caller/__init__.py
@@ -26,14 +26,13 @@ __all__ = ['corsika_iact']
 
 import docopt
 import os
-from os.path import join
 import glob
 import subprocess
 import sys
 import random
 
 def all_files_in(path):
-    return glob.glob(join(path, '*'))
+    return glob.glob(os.path.join(path, '*'))
 
 def mkdir(path):
     subprocess.call(['mkdir', path])
@@ -62,11 +61,23 @@ def extract_output_path_from(line):
     second_quote = line[first_quote+1:].find('"') + first_quote+1
     return line[first_quote+1:second_quote]
 
+class CanNotFindTelfilKeyWordInCorsikaInputCard(Exception):
+    pass
+
 def output_path_taken_from_corsika_input_card(input_card_path):
+    output_path = ""
+    found_TELFIL_keyword = False
     with open(input_card_path) as fileobject:
         for line in fileobject:
             if supposed_to_store_output_path(line):
-                return extract_output_path_from(line)
+                found_TELFIL_keyword = True
+                output_path = extract_output_path_from(line)
+
+    if found_TELFIL_keyword:
+        return output_path
+    else:
+        raise CanNotFindTelfilKeyWordInCorsikaInputCard
+
 
 def corsika_iact(corsika_path, input_card_path, save_stdout=False):
     """
@@ -109,28 +120,32 @@ def corsika_iact(corsika_path, input_card_path, save_stdout=False):
 
     temp_working_dir = out.basename_wo_extension+'_temp_'+str(random.randint(0,1e6))
     
-    if join(out.dirname, temp_working_dir) not in all_files_in(out.dirname):
-        mkdir(join(out.dirname, temp_working_dir))
-
+    if os.path.join(out.dirname, temp_working_dir) not in all_files_in(out.dirname):
+        mkdir(os.path.join(out.dirname, temp_working_dir))
 
         # symlink all Corsika relevant files to temp working dir
         for f in all_files_in(corsika.dirname):
             ff = Path(f)
             symlink(
-            	join(ff.dirname, ff.basename), 
-                join(out.dirname, temp_working_dir, ff.basename)
+            	os.path.join(ff.dirname, ff.basename), 
+                os.path.join(out.dirname, temp_working_dir, ff.basename)
             )
 
         # symlink all IACT Corsika relevant files to temp working dir
         corsika_main_path = os.path.split(corsika.dirname)[0]
-        corsika_iact_path = join(corsika_main_path, 'bernlohr')
+        corsika_iact_path = os.path.join(corsika_main_path, 'bernlohr')
         
         for iact_path in all_files_in(corsika_iact_path):
             iact_file = Path(iact_path)
             if iact_file.basename_wo_extension[0:7] == 'atmprof':
                 symlink(
-                    join(iact_file.dirname, iact_file.basename), 
-                    join(out.dirname, temp_working_dir, iact_file.basename)
+                    os.path.join(
+                        iact_file.dirname, 
+                        iact_file.basename), 
+                    os.path.join(
+                        out.dirname,
+                        temp_working_dir,
+                        iact_file.basename)
                 )
 
         # call corsika in the temporary working diractory and 
@@ -138,33 +153,42 @@ def corsika_iact(corsika_path, input_card_path, save_stdout=False):
 
         if save_stdout:
             # pipe the stdout and stderr into files
-            corsika_stdout = open(join(out.dirname, out.basename_wo_extension+'_stdout.txt'), 'w')
-            corsika_stderr = open(join(out.dirname, out.basename_wo_extension+'_stderr.txt'), 'w')
+            corsika_stdout = open(
+                os.path.join(
+                    out.dirname, 
+                    out.basename_wo_extension+'_stdout.txt'), 
+                'w')
+            corsika_stderr = open(
+                os.path.join(
+                    out.dirname, 
+                    out.basename_wo_extension+'_stderr.txt'), 
+                'w')
 
             corsika_return_value = subprocess.call(
-                join(out.dirname, temp_working_dir, corsika.basename), 
+                os.path.join(out.dirname, temp_working_dir, corsika.basename), 
                 stdin=input_card_file, 
                 stdout=corsika_stdout, 
                 stderr=corsika_stderr,
-                cwd=join(out.dirname, temp_working_dir)
+                cwd=os.path.join(out.dirname, temp_working_dir)
             )
 
             corsika_stderr.close()
             corsika_stdout.close()
         else:
             corsika_return_value = subprocess.call(
-                join(out.dirname, temp_working_dir, corsika.basename), 
+                os.path.join(out.dirname, temp_working_dir, corsika.basename), 
                 stdin=input_card_file,
-                cwd=join(out.dirname, temp_working_dir)
+                cwd=os.path.join(out.dirname, temp_working_dir)
             )
 
         input_card_file.close()
 
         # remove the temporary working directory
-        rm_dir(join(out.dirname, temp_working_dir))
+        rm_dir(os.path.join(out.dirname, temp_working_dir))
 
         # remove write protection from corsika output
-        subprocess.call(['chmod', '+w', join(out.dirname, out.basename)])
+        subprocess.call(
+            ['chmod', '+w', os.path.join(out.dirname, out.basename)])
 
         return corsika_return_value
 
@@ -181,7 +205,7 @@ def main():
         sys.exit(corsika_return_value)
 
     except docopt.DocoptExit as e:
-        print(e.message)
+        print(e)
 
 if __name__ == '__main__':
     main()
