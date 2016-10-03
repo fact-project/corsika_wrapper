906ebd2028e27b7a69b29e70da3d4eab4d130bc2
sebastian <sebmuell@phys.ethz.ch>
2016-10-03 12:41:59 +0200

    call it wrapper everywhere

--- corsika_wrapper/tools.py
+++ corsika_wrapper/tools.py
@@ -50,7 +50,7 @@ def get_home_path():
 
 
 def get_config_dir_path():
-    return os.path.join(get_home_path(), '.corsika_caller')
+    return os.path.join(get_home_path(), '.corsika_wrapper')
 
 
 def get_config_file_path():
