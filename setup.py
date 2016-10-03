600f43593d2f3e8d44f590d50d709c1cb0ddc464
sebastian <sebmuell@phys.ethz.ch>
2016-10-03 11:44:52 +0200

    its not only an iact caller

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup
 
 setup(
-    name='corsika_iact_caller',
-    version='0.0.1',
-    description='call the CORSIKA executable with IACT package in a modern and thread safe way',
-    url='https://github.com/fact-project/corsika_iact_caller.git',
-    author='Sebastian Mueller',
+    name='corsika_caller',
+    version='1.0.0',
+    description='call the CORSIKA executable in thread safe and comfortable way',
+    url='https://github.com/fact-project/corsika_caller.git',
+    author='Sebastian Achim Mueller',
     author_email='sebmuell@phys.ethz.ch',
     license='MIT',
     packages=[
-        'corsika_iact_caller',
+        'corsika_caller',
     ],
     install_requires=[
         'docopt'
     ],
     entry_points={'console_scripts': [
-        'corsika_iact = corsika_iact_caller.__init__:main',
+        'corsika = corsika_caller.__init__:main',
     ]},
     zip_safe=False,
 )
