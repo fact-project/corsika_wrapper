1d2d9a6975dc9e108036a5de3935a5682a27979c
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-10-09 14:36:45 +0200

    add classifiers, add readme, drop short licence entry

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import setuptools
 import os
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='corsika_wrapper',
     version='1.0.1',
     description='Call CORSIKA in a thread safe and comfortable way.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/fact-project/corsika_wrapper.git',
     author='Sebastian Achim Mueller',
     author_email='sebastian-achim.mueller@mpi-hd.mpg.de',
-    license='GPL v3',
     packages=[
         'corsika_wrapper',
     ],
@@ -19,4 +23,13 @@ setuptools.setup(
     entry_points={'console_scripts': [
         'corsika = corsika_wrapper.main:main',
     ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
 )
